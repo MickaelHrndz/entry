222e62666d2f41677a941006b788409051e65e25
Mickaël Hernandez <mickael.hernandez9@gmail.com>
2021-04-26 23:15:34 +0200

    adding support for MacOS & Linux

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import entry
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  EntryPlugin.register(with: registry.registrar(forPlugin: "EntryPlugin"))
+}
