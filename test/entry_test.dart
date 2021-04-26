cadc54e27cc920ae430e057bae6618c53e00033e
Mickaël Hernandez <mickael.hernandez9@gmail.com>
2021-04-26 22:48:45 +0200

    adding tests

--- test/entry_test.dart
+++ test/entry_test.dart
@@ -6,8 +6,8 @@ void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   /// custom function to wrap [WidgetTester.pumpWidget]
-  Future<void> pumpEntry(WidgetTester tester, Entry Entry) =>
-      tester.pumpWidget(MaterialApp(home: Entry), Duration(seconds: 1));
+  Future<void> pumpEntry(WidgetTester tester, Entry entry) =>
+      tester.pumpWidget(MaterialApp(home: entry), Duration(seconds: 1));
 
   testWidgets('default constructor', (tester) async {
     await pumpEntry(tester, Entry(child: Text('test')));
@@ -27,5 +27,37 @@ void main() {
   testWidgets('move constructor', (tester) async {
     await pumpEntry(tester, Entry.offset(child: Text('test')));
     expect(find.text('test'), findsOneWidget);
+  });
+  
+  testWidgets('default values are used when not specified', (tester) async {
+      await tester.pumpWidget(Entry(child: FlutterLogo()));
+      //tester.idle();
+      var entry = tester.widget<Entry>(find.byType(Entry));
+      expect(entry.delay, Duration.zero);
+      expect(entry.duration, Duration(milliseconds: 300));
+      expect(entry.curve, Curves.ease);
+      expect(entry.opacity, 1);
+      expect(entry.scale, 1);
+      expect(entry.xOffset, 0);
+      expect(entry.yOffset, 0);
+      expect(entry.angle, 0);
+  });
+  
+  testWidgets('assertion error if opacity is out of bounds', (tester) async {
+      // opacity < 0 throws assertion error
+      expect(() async => await tester.pumpWidget(
+        Entry(
+          opacity: -1,
+          child: FlutterLogo()
+        )
+      ), throwsAssertionError);
+
+      // opacity > 1 throws assertion error
+      expect(() async => await tester.pumpWidget(
+        Entry(
+          opacity: 1.01,
+          child: FlutterLogo()
+        )
+      ), throwsAssertionError);
   });
 }
