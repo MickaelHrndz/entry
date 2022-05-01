fbc21ac5d4a8a1014e4a2abfdd31574ec8725705
Mickaël Hernandez <mickael.hernandez9@gmail.com>
2022-05-01 04:05:58 +0200

    add visible parameter to Entry

--- lib/entry.dart
+++ lib/entry.dart
@@ -15,6 +15,9 @@ class Entry extends StatelessWidget {
   /// The animation curve
   final Curve curve;
 
+  /// The state of visibility
+  bool visible;
+
   /// The initial opacity (goes from [opacity] to 1)
   final double opacity;
 
@@ -31,26 +34,28 @@ class Entry extends StatelessWidget {
   final double angle;
 
   /// Default constructor (motionless by default)
-  const Entry({
+  Entry({
     Key? key,
     this.delay = Duration.zero,
     this.duration = const Duration(milliseconds: 300),
     this.curve = Curves.ease,
+    this.visible = true,
     this.opacity = 1,
     this.scale = 1,
     this.xOffset = 0,
     this.yOffset = 0,
     this.angle = 0,
     required this.child,
-  })   : assert(opacity >= 0 && opacity <= 1),
+  })  : assert(opacity >= 0 && opacity <= 1),
         super(key: key); // coverage:ignore-line
 
   /// Constructor making use of every animation by default except [angle]
-  const Entry.all({
+  Entry.all({
     Key? key,
     this.delay = Duration.zero,
     this.duration = const Duration(milliseconds: 300),
     this.curve = Curves.ease,
+    this.visible = true,
     this.opacity = 0,
     this.scale = 0,
     this.xOffset = 0,
@@ -60,62 +65,65 @@ class Entry extends StatelessWidget {
   }) : super(key: key); // coverage:ignore-line
 
   /// Opacity-only constructor
-  const Entry.opacity({
+  Entry.opacity({
     Key? key,
     Duration delay = Duration.zero,
     Duration duration = const Duration(milliseconds: 300),
     Curve curve = Curves.ease,
+    bool visible = true,
     double opacity = 0,
     required Widget child,
-  }) : this( // coverage:ignore-line
+  }) : this(
+          // coverage:ignore-line
           key: key,
           delay: delay,
           duration: duration,
           curve: curve,
+          visible: visible,
           opacity: opacity,
           child: child,
-          yOffset: 0,
-          scale: 1,
         );
 
   /// Scale-only constructor
-  const Entry.scale({
+  Entry.scale({
     Key? key,
     Duration delay = Duration.zero,
     Duration duration = const Duration(milliseconds: 300),
     Curve curve = Curves.ease,
+    bool visible = true,
     double scale = 0,
     required Widget child,
-  }) : this( // coverage:ignore-line
+  }) : this(
+          // coverage:ignore-line
           key: key,
           delay: delay,
           duration: duration,
           curve: curve,
+          visible: visible,
           scale: scale,
           child: child,
-          yOffset: 0,
-          opacity: 1,
         );
 
   /// Offset-only constructor
-  const Entry.offset({
+  Entry.offset({
     Key? key,
     Duration delay = Duration.zero,
     Duration duration = const Duration(milliseconds: 300),
     Curve curve = Curves.ease,
+    bool visible = true,
     double xOffset = 0,
     double yOffset = 1000,
     required Widget child,
-  }) : this( // coverage:ignore-line
+  }) : this(
+          // coverage:ignore-line
           key: key,
           delay: delay,
           duration: duration,
           curve: curve,
+          visible: visible,
           xOffset: xOffset,
           yOffset: yOffset,
           child: child,
-          opacity: 1,
-          scale: 1,
         );
 
   @override
@@ -126,17 +134,18 @@ class Entry extends StatelessWidget {
       ..add("xOffset", Tween(begin: xOffset, end: 0.0), duration, curve)
       ..add("yOffset", Tween(begin: yOffset, end: 0.0), duration, curve)
       ..add("angle", Tween(begin: angle, end: 0.0), duration, curve);
-    return PlayAnimation<MultiTweenValues<String>>(
+    return CustomAnimation<MultiTweenValues<String>>(
+      control: visible
+          ? CustomAnimationControl.play
+          : CustomAnimationControl.playReverse,
       delay: delay,
       duration: tween.duration,
       tween: tween,
       child: child,
       builder: (context, child, value) => Transform.scale(
         scale: value.get("scale"),
-        child: AnimatedOpacity(
+        child: Opacity(
           opacity: value.get("opacity"),
-          curve: curve,
-          duration: duration,
           child: Transform.translate(
             offset: Offset(
               value.get("xOffset"),
